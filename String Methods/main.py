111bd450b15f1020b24e1420cfbda5a1d0792203
TRINITY2498 <kaustubhk033@gmail.com>
2025-12-27 23:59:51 +0530

    String Methods Added.

---
+++ String Methods/main.py
@@ -0,0 +1,82 @@
+message = "-R-a-v-i-"
+
+ext_slicing = message[1:8:2]
+
+print(ext_slicing)
+
+"""
+pin = "1234"
+
+is_digit = pin.isdigit()
+is_4_digit = (len(pin) == 4)
+
+is_valid = is_digit and is_4_digit
+
+print(is_valid)
+
+"""
+
+"""
+mobile_1 = " 7058438724 "
+mobile_2 = "7058438724"
+
+is_same = (mobile_1 == mobile_2)
+
+print(is_same)
+
+"""
+
+"""
+mobile = " 7058438724 "
+
+mobile = mobile.strip()
+
+print(mobile)
+
+"""
+"""
+name = "Ravi."
+name = name.strip(".")
+
+print(name)
+
+"""
+
+"""
+sentence = "teh cat and teh dog"
+sentence = sentence.replace("teh","the")
+print(sentence)
+
+"""
+
+"""
+url = "https://onthegomodel.com"
+is_secure_url = url.startswith("https://")
+
+print(is_secure_url)
+
+"""
+
+"""
+gmail_id = "rahul123@gmail.com"
+is_gmail = gmail_id.endswith("@gmail.com")
+
+print(is_gmail)
+
+"""
+
+"""
+name = "ravi"
+name = name.upper()
+
+print(name)
+
+"""
+
+"""
+name = "RAVI"
+name = name.lower()
+
+print(name)
+
+"""
