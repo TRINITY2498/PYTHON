02338457c57305981e7ae7b743ca6a2b44f67471
TRINITY2498 <kaustubhk033@gmail.com>
2026-01-02 18:04:22 +0530

    Problem Solving and Debugging - Part 3 Added.

---
+++ Problem Solving and Debugging - Part 3/Q1.py
@@ -0,0 +1,21 @@
+s = input()
+
+n = len(s)
+result = ""
+for i in range(n):
+    result = result + s[i]
+    print(result)
+
+
+# Second method of code
+
+"""
+
+s = input()
+
+n = len(s)
+result = ""
+for i in range(1,n + 1):
+    print(s[:i])
+
+"""
---
+++ Problem Solving and Debugging - Part 3/Q2.py
@@ -0,0 +1,9 @@
+s = input()
+result = ""
+
+for i in range(len(s)):
+    a = int(input())
+    
+    result = result + s[a]
+
+print(result)
