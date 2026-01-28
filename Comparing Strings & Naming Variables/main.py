591e585bd9441e84f4c1c155e98120ffb33c0c03
TRINITY2498 <kaustubhk033@gmail.com>
2026-01-28 15:23:38 +0530

    Coparing Strings & Naming Variables Session Added.

---
+++ Comparing Strings & Naming Variables/main.py
@@ -0,0 +1,40 @@
+"""
+
+unicode_value = ord("A")
+
+print(unicode_value)
+
+"""
+
+"""
+
+char = chr(75)
+
+print(char)
+
+"""
+
+"""
+
+for unicode_value in range(65,91):
+    print(str(unicode_value) + " : " + str(chr(unicode_value)))
+
+"""
+
+"""
+
+print("a" > "A")
+
+print("9" > "5")
+
+print("BAD" >= "BAT")
+
+print("98" < "984")
+
+"""
+
+"""
+
+help("keywords")
+
+"""
