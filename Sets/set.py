38dfb740e26381dae3318d11efb80dc860df897c
TRINITY2498 <kaustubhk033@gmail.com>
2026-04-28 23:55:05 +0530

    Sets Added.

---
+++ Sets/set.py
@@ -0,0 +1,84 @@
+'''
+
+a = 2 
+
+set_a = {5, "Six", a, 8.2}
+
+print(type(set_a))
+
+print(set_a)
+
+'''
+
+'''
+
+set_a = set()
+
+print(type(set_a))
+
+print(set_a)
+
+'''
+
+'''
+set_a = set([1,2,1])
+
+print(type(set_a))
+
+print(set_a)
+
+'''
+
+'''
+set_a = {1, 3, 6, 2, 9}
+
+set_a.add(7)
+
+print(set_a)
+
+'''
+
+'''
+set_a = {1, 3, 6, 2, 9}
+
+set_a.update([99, 100])
+
+print(set_a)
+
+'''
+
+'''
+set_a = {1, 3, 6, 2, 9}
+
+set_a.discard(3)
+
+print(set_a)
+
+'''
+
+'''
+set_a = {1, 3, 6, 2, 9}
+
+set_a.clear()
+
+print(set_a)
+
+'''
+
+'''
+set_a = {1, 3, 6, 2, 9}
+
+length = len(set_a)
+
+print(length)
+
+'''
+
+'''
+set_a = {1, 3, 6, 2, 9}
+
+is_part = 5 not in set_a
+
+print(is_part)
+
+'''
