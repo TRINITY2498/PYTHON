f6f5f18d6c342e0b850fce4afaf66633f3d69c3a
TRINITY2498 <kaustubhk033@gmail.com>
2026-08-14 23:40:32 +0530

    Problem Solving and Debbuging.

---
+++ Problem Solving and Debugging - Part 7/Q1.py
@@ -0,0 +1,8 @@
+# Find the Mistake Problem
+
+def student_info(*args, **kwargs):
+    
+    print(args)
+    print(kwargs)
+    
+student_info('Math', 'Science', name = 'Rahul', age = '15')
---
+++ Problem Solving and Debugging - Part 7/Q3.py
@@ -0,0 +1,32 @@
+# Grouping Of Scores.
+
+
+def get_scores(ball_score_list):
+    
+    ball_score_dict = {}
+
+    for item in ball_score_list:
+        
+        pair = item.split(":")
+        
+        key, value = pair[0], int(pair[1])
+        
+        if key in ball_score_dict:
+            
+            score = ball_score_dict[key]
+            
+            ball_score_dict[key] = score + value
+        
+        else:
+            
+            ball_score_dict[key] = value 
+        
+    return ball_score_dict
+
+
+
+ball_score_list = input().split(",")
+
+ball_score_pairs = get_scores(ball_score_list)
+
+print(ball_score_pairs)
